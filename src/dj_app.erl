d8e05e5590b487ee3b6378b717ae4b761bb0d91d
Johan <johan@ThinkPad-T61.(none)>
2012-05-11 12:21:27 +0200

    Initial commit

---
+++ src/dj_app.erl
@@ -0,0 +1,16 @@
+-module(dj_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    dj_sup:start_link().
+
+stop(_State) ->
+    ok.
